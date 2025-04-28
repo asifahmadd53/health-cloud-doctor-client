8315263d6ec1d4047d4d46201d8a8fac7b4065a0
asifahmadd53 <asifahmadd53@gmail.com>
2025-04-28 22:28:32 +0500

    installing some code

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import '../gesture-handler.native';
 
 const RootLayout = () => {
   return (
    <>
-   <View>
+   <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#FFFFFF"
+        translucent={false}
+      />
+    <NavigationContainer>
     <Text className='text-3xl bg-red-700'>Hello, World!</Text>
-   </View>
+    </NavigationContainer>
    </>
   )
 }
