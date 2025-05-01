d117ba45d00a3db12b121dc584c1e3ef5445b69f
asifahmadd53 <asifahmadd53@gmail.com>
2025-05-01 19:52:38 +0500

    final shifted success

--- client/src/navigation/Main/NewAppointment.tsx
+++ client/src/navigation/Main/NewAppointment.tsx
@@ -1,9 +1,7 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NewAppointment } from '../../screens';
-import Icon from 'react-native-vector-icons/MaterialIcons';
 
 
 
