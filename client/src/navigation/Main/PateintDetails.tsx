649b632f0d08a8c1aca4f16dfb57f7ebc6f21066
asifahmadd53 <asifahmadd53@gmail.com>
2025-05-01 19:52:48 +0500

    final shifted success

--- client/src/navigation/Main/PateintDetails.tsx
+++ client/src/navigation/Main/PateintDetails.tsx
@@ -1,4 +1,3 @@
-import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { PatientDetails, PreviouseData } from '../../screens';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
