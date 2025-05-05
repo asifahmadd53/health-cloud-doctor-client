81f40f2a5ad76a09770ceea4371e0cfbc876554b
asifahmadd53 <asifahmadd53@gmail.com>
2025-05-05 23:25:08 +0500

    adding slider

--- client/src/screens/Auth/SignIn.tsx
+++ client/src/screens/Auth/SignIn.tsx
@@ -4,44 +4,72 @@ import {
   View,
   KeyboardAvoidingView,
   Alert,
+  ActivityIndicator,
+  Pressable,
+  Animated,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import CustomInput from "../../components/CustomInput";
 import CustomPasswordInput from "../../components/CustomPasswordInput";
-import CustomButton from "../../components/CustomButton";
 import Icons from "../../utils/libs/constants/Icons";
 import axios from "axios";
 import { API_URL } from "../../utils/libs/constants/api/api";
 
-
-
-
-
 const SignIn = () => {
+  const [pmdcNumber, setpmcd] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation<any>();
 
-  const [pmdcNumber, setpmcd] = useState('')
-  const [password, setPassword] = useState('')
+  const [isLoading, setLoading] = useState(false);
 
-  const navigation: any = useNavigation()
+  const fadeOut = useRef(new Animated.Value(0)).current;
 
-  // const handleSignIn = async()=>{
-  //   if(!pmdcNumber || !password){
-  //     Alert.alert('Please fill all the fields')
-  //     return
+  const _fadeOut = fadeOut.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 0.5, 0],
+  });
+
+  // const handleLogin = async () => {
+  //   if (!pmdcNumber || !password) {
+  //     Alert.alert("Please fill all the fields");
+  //     return;
   //   }
-  //   const formData = new FormData();
-  //   formData.append('pmdcNumber',pmdcNumber)
-  //   formData.append('password',password)
-  //   const response = await axios.post(`${API_URL}/api/auth/login`,formData)
-  //   if(response.data.success){
-  //     Alert.alert('Login successful')
-  //     navigation.navigate('drawer')
-  //   }else{
-  //     Alert.alert(response.data.message)
+
+  //   try {
+  //     setLoading(true);
+  //     Animated.timing(fadeOut, {
+  //       toValue: 1,
+  //       duration: 250,
+  //       useNativeDriver: false,
+  //     }).start();
+
+
+  //     const response = await axios.post(`${API_URL}/api/auth/login`, {
+  //       pmdcNumber,
+  //       password,
+  //     },
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     }
+  //   );
+
+      
+  //   if (response.data.success) {
+  //     navigation.navigate("drawer");
+  //   } else {
+  //     Alert.alert(response.data.message);
   //   }
-  // }
+    
+  //   } catch (error) {
+  //     Alert.alert("Something went wrong. Please try again.");
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <SafeAreaView className="bg-white px-3">
@@ -56,32 +84,59 @@ const SignIn = () => {
               relationships with unparalleled ease.
             </Text>
           </View>
+
           <View className="mt-16 w-[97%] mx-auto gap-5">
-           <Text className="text-base md:text-lg">Enter your PMDC #</Text>
-            <CustomInput placeholder={'PMDC #'} icon={Icons.tick} value={pmdcNumber} onChange={setpmcd} />
+            <Text className="text-base md:text-lg">Enter your PMDC #</Text>
+            <CustomInput
+              placeholder={"PMDC #"}
+              icon={Icons.tick}
+              value={pmdcNumber}
+              onChange={setpmcd}
+            />
             <Text className="text-base md:text-lg">Enter your Password</Text>
-            <CustomPasswordInput placeholder={'Password'} value={password} onChange={setPassword} />
+            <CustomPasswordInput
+              placeholder={"Password"}
+              value={password}
+              onChange={setPassword}
+            />
 
             <View className="items-end">
-
-              <Text onPress={() => navigation.navigate('forget-password')} className="text-secondary text-base font-medium">
+              <Text
+                onPress={() => navigation.navigate("forget-password")}
+                className="text-secondary text-base font-medium"
+              >
                 Forgot Password?
               </Text>
-
             </View>
           </View>
 
-
-          <View className="mt-6">
-            <CustomButton label={'Login'} link={'drawer'} onPress={()=>{}}/>
+          <View className="mt-6 items-center">
+            <Pressable
+              // onPress={handleLogin}
+              onPress={() => navigation.navigate("drawer")}
+              className="bg-secondary w-[70%] mx-auto rounded-full py-3 md:py-5 h-[50px] justify-center items-center"
+              >
+              {isLoading ? (
+                <ActivityIndicator size="small" color="white" />
+              ) : (
+                <Animated.Text
+                 className='text-white text-xl text-center font-semibold'
+                >
+                  Submit
+                </Animated.Text>
+              )}
+            </Pressable>
           </View>
+
           <View className="mx-auto mt-8 items-center gap-5">
-            <Text onPress={() => navigation.navigate('sign-up')} className="text-[#253237] text-base font-medium">
-              Don’t have an account?<Text className="text-secondary"> Join us</Text>
+            <Text
+              onPress={() => navigation.navigate("sign-up")}
+              className="text-[#253237] text-base font-medium"
+            >
+              Don’t have an account?
+              <Text className="text-secondary"> Join us</Text>
             </Text>
           </View>
-          {/* ✅ Modal 1 */}
-
         </KeyboardAvoidingView>
       </ScrollView>
     </SafeAreaView>
--- client/src/screens/NewAppoinment/index.tsx
+++ client/src/screens/NewAppoinment/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RadioButton } from 'react-native-paper';
@@ -13,17 +13,20 @@ const NewAppointment = () => {
   const [mobile, setMobile] = useState('');
   const [gender, setGender] = useState<'male' | 'female' | null>(null);
   const [paymentMethod, setPaymentMethod] = useState<'cash' | 'online' | null>(null);
+  const [age, setAge] = useState('');
 
   return (
     <SafeAreaView className='px-5 py-5 flex-1 bg-white'>
       <Header title=''/>
       <Text className='text-2xl font-bold text-gray-800 text-center mb-6 mt-6'>New Appointment</Text>
 
-      <View className='bg-white rounded-2xl p-5 shadow-md'>
+      <ScrollView>
+      <View className='bg-white rounded-2xl p-5 shadow-md border border-gray-200'>
         {/* Patient Details */}
         <CustomSimpleInput value={name} onChange={setName} placeholder="Enter patient name" label="Patient Name" />
         <CustomSimpleInput value={CNIC} onChange={setCNIC} placeholder="Enter patient CNIC #" label="Patient CNIC #" />
         <CustomSimpleInput value={mobile} onChange={setMobile} placeholder="Enter patient Mobile #" label="Mobile #" />
+        <CustomSimpleInput value={age} onChange={setAge} placeholder="Enter patient age" label="Age" />
 
         {/* Gender Selection */}
         <Text className='mt-4 mb-2 font-semibold text-lg text-gray-700'>Gender</Text>
@@ -68,6 +71,7 @@ const NewAppointment = () => {
           <CustomButton label='Proceed'/>
         </View>
       </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
